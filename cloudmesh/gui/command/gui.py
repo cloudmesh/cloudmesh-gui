ee27239dcbeabb2b043893e2e06a21949b54bb0d
Gregor von Laszewski <laszewski@gmail.com>
2020-02-21 10:11:18 -0500

    add quick gui for class quick checks

--- cloudmesh/gui/command/gui.py
+++ cloudmesh/gui/command/gui.py
@@ -19,6 +19,7 @@ class GuiCommand(PluginCommand):
                 gui mongo [user]
                 gui cloud CLOUD [--show]
                 gui edit KEY [--show]
+                gui quick
 
 
           This command allows configuration of cloudmesh with a GUI
@@ -26,6 +27,34 @@ class GuiCommand(PluginCommand):
           Options:
               -f      specify the file
               --show  shows also the password
+
+          Description:
+                gui activate
+
+                  activates clouds by selecting them
+
+                gui profile
+
+                  lets you fill out the profile
+
+                gui mongo [user]
+
+                  lets you fill out the mongo information
+
+                gui cloud CLOUD [--show]
+
+                  lets you fill out the specific cloud
+
+                gui edit KEY [--show]
+
+                  lets you fill out any data identified by the Key in the yaml
+                  file in dot notation
+
+                gui quicke
+
+                  lets you fill out a combination that is good enough to get
+                  you started in class (this is experiemental)
+
         """
 
         arguments.show = arguments["--show"]
@@ -40,8 +69,8 @@ class GuiCommand(PluginCommand):
         elif arguments.mongo and arguments.user:
             Gui.edit_list(["cloudmesh.data.mongo.MONGO_USERNAME",
                            "cloudmesh.data.mongo.MONGO_PASSWORD"],
-                     caps=False,
-                     show=arguments.show)
+                          caps=False,
+                          show=arguments.show)
 
         elif arguments.mongo:
             Gui.edit(f"cloudmesh.data.mongo",
@@ -59,5 +88,22 @@ class GuiCommand(PluginCommand):
             if not key.startswith("cloudmesh."):
                 key = "cloudmesh." + key
             Gui.edit(key, caps=False, show=arguments.show)
+
+
+        elif arguments.quick:
+            Gui.edit_list([
+                "cloudmesh.profile.firstname",
+                "cloudmesh.profile.lastname",
+                "cloudmesh.profile.email",
+                "cloudmesh.profile.user",
+                "cloudmesh.profile.github",
+                "cloudmesh.data.mongo.MONGO_USERNAME",
+                "cloudmesh.data.mongo.MONGO_PASSWORD",
+                "cloudmesh.data.mongo.MODE",
+                "cloudmesh.cloud.chameleon.credentials.auth.username",
+                "cloudmesh.cloud.chameleon.credentials.auth.password",
+            ],
+            caps=False,
+            show=arguments.show)
 
         return ""
